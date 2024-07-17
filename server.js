d852062af61bcb444649195ad289f1686361e3c2
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-07-17 15:40:51 +0200

    the last edit

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
 
-// sendFile will go here
-app.get('/', (req, res)=> {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.set("view engine", "html");
+
+app.set("views", path.join(__dirname, ""));
+
+app.use(express.static(path.join(__dirname, "")));
+
+app.get("/", (req, res) => {
+
+  res.render("index", data);
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
