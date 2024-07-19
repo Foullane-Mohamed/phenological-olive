260e44f74d5db7cd9f8895b36cf5e50e12064b37
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-07-19 17:24:48 +0200

    add slider botstram but i dont edit it

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 let humbergerBar = document.querySelector('.humberger');
 let nav_menu = document.querySelector('.nav_menu');
-let nav_itmes = document.querySelectorAll('.nav_item');
+let nav_items = document.querySelectorAll('.nav_item');
 
 humbergerBar.addEventListener('click', () => {
   humbergerBar.classList.toggle('active');
   nav_menu.classList.toggle('active');
 });
 
-nav_itmes.forEach((e) => {
+nav_items.forEach((e) => {
   e.addEventListener('click', () => {
-    nav_itmes.forEach((el) => {
+    nav_items.forEach((el) => {
       el.classList.remove('active_color');
     });
     e.classList.add('active_color');
