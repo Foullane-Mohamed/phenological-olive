4e5b222f4df0c7548127ecf75263b412a5d4a279
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-07-19 15:51:07 +0200

    version 2 foter

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,19 @@
-let humbergerBar = document.querySelector('.humberger')
-let nav_menu = document.querySelector('.nav_menu')
-let nav_itmes = document.querySelectorAll('.nav_item')
+let humbergerBar = document.querySelector('.humberger');
+let nav_menu = document.querySelector('.nav_menu');
+let nav_itmes = document.querySelectorAll('.nav_item');
 
-humbergerBar.addEventListener('click',()=>{
-  humbergerBar.classList.toggle('active')
-  nav_menu.classList.toggle('active')
-})
+humbergerBar.addEventListener('click', () => {
+  humbergerBar.classList.toggle('active');
+  nav_menu.classList.toggle('active');
+});
 
-
-nav_itmes.forEach((e)=>{
-  e.addEventListener('click',()=>{
-  nav_itmes.forEach((el)=>{
-    el.classList.remove('active_color')
-  })
-  e.classList.add('active_color')
-  humbergerBar.classList.remove('active')
-  nav_menu.classList.remove('active')
-  })
-})
+nav_itmes.forEach((e) => {
+  e.addEventListener('click', () => {
+    nav_itmes.forEach((el) => {
+      el.classList.remove('active_color');
+    });
+    e.classList.add('active_color');
+    humbergerBar.classList.remove('active');
+    nav_menu.classList.remove('active');
+  });
+});
